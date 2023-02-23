fbc1be506f4cc50a293cbdaaa7c9e32f3a8371c6
soniagaber <soniagaber90@gmail.com>
2023-02-23 20:22:38 +0100

    finito

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let nome;
+let cognome;
+let colore;
+let numeroScelto = 4;
+
+nome=prompt("qual è il tuo nome?");
+cognome=prompt("qual è il tuo cognome?");
+colore=prompt("qual è il tuo colore preferito?");
+document.writeln (nome+cognome+colore+numeroScelto);
